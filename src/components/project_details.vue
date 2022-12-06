548871eaa01fa77a4c9a567f1842c8f74d808e4e
JoJoPuppe <marcusloeper@gmx.de>
2022-12-06 21:29:13 +0100

    added remix icons

--- src/components/project_details.vue
+++ src/components/project_details.vue
@@ -1,24 +1,31 @@
 <template>
-
-  <div class="text-primary" v-html="markdownHTML">
-  </div>
-
+  <editor-content :editor="editor" />
 </template>
 
 <script>
-import { marked } from 'marked';
+import { Editor, EditorContent } from "@tiptap/vue-3";
+import StarterKit from "@tiptap/starter-kit";
 
 export default {
-  data(){
-    return {
-      markdown: "# This Project"
-    }
+  components: {
+    EditorContent,
   },
-  computed: {
-    markdownHTML(){
-      return marked(this.markdown)
-    }
-  }
 
-}
+  data() {
+    return {
+      editor: null,
+    };
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      content: "<p>I’m running Tiptap with Vue.js. 🎉</p>",
+      extensions: [StarterKit],
+    });
+  },
+
+  beforeUnmount() {
+    this.editor.destroy();
+  },
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import axios from "axios";
-import VueAxios from "vue-axios";
 import router from "./router/index.js";
 import "./style.css";
 import App from "./App.vue";
