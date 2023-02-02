f3bbbc71f56a2839374b88d5804a91e1a87bec48
jojopuppe <marcusloeper@gmx.de>
2023-02-02 09:39:39 +0100

    ui update project list

--- src/components/admin_all_projects.vue
+++ src/components/admin_all_projects.vue
@@ -1,12 +1,43 @@
 <template>
-  <div>
-
-  </div>
+    <div class="bg-primary h-16 flex justify-between items-center p-4">
+      <div class="flex items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 80.81 107.23"
+          class="fill-base-100 w-10 -ml-2 -mt-1"
+        >
+          <g>
+            <path
+              d="M52.85,68.14H36.17v30.11h-5.17c-6.74,0-12.21-5.47-12.21-12.21V17.18h34.05c6.78,0,12.28,5.5,12.28,12.28
+            v26.41C65.13,63.16,60.26,68.14,52.85,68.14z M47.41,33.39H36.17v18.53h11.24c0.19,0,0.35-0.16,0.35-0.35V33.74
+            C47.76,33.55,47.6,33.39,47.41,33.39z"
+            />
+          </g>
+        </svg>
+        <div class="mx-4">
+          <p class="font-bold ">Asset Tracker</p>
+          <p class="font-bold text-lg">Project List</p>
+        </div>
+    </div>
+</div>
+  <ul class="w-1/3">
+    <li v-for="project in projects">
+      <ProjectItem :projectobj="project"/>
+    </li>
+  </ul>
 </template>
 <script>
 import axios from "axios";
-
+import ProjectItem from './project_item.vue'
 export default {
+  data() {
+    return {
+      projects: [],
+    }
+  },
+  components: {
+    ProjectItem,
+  },
   mounted() {
     this.get_all_projects();
   },
@@ -16,11 +47,11 @@ export default {
         url: "/project",
         baseURL: import.meta.env.VITE_BASEURL,
         auth: {
-          username: "jojop",
-          password: "jojo",
+          username: "pharos",
+          password: "pharos",
         }
       }).then((response) => {
-        console.log(response.data);
+        this.projects = response.data;
       });
     }
   }
---
+++ src/components/project_item.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="rounded-xl bg-primary p-3 m-2 broder border-2 hover:border-accent">
+      <button @click="delete_log_message" class="btn btn-sm btn-square fill-gray-500 hover:fill-accent mr-4">
+        <RiDeleteBinFill class="w-5 h-5" />
+      </button>
+    <a
+      class="link link-hover text-xl font-bold text-accent"
+      :href="`${baseurl}/prj/${projectobj._id}`"
+      >{{ projectobj.name }}</a
+    >
+    <p class="text-sm text-gray-400">creation date: {{ projectobj.creation_date }}</p>
+  </div>
+</template>
+<script>
+import RiPencilFill from "vue-remix-icons/icons/RiPencilFill.vue";
+import RiDeleteBinFill from "vue-remix-icons/icons/RiDeleteBinFill.vue";
+export default {
+  components: {
+    RiPencilFill,
+    RiDeleteBinFill
+  },
+  props: ['projectobj'],
+  data() {
+    return {
+      baseurl: import.meta.env.VITE_ASSETBASE,
+    }
+  },
+}
+</script>
